1e749ea4b5a031bb7167245918544cdfa9a247ca
Ian Meyer <ianmeyer@mac.com>
2013-12-10 20:12:15 -0500

    🍻

--- 12Daze/DazeEmitterView.m
+++ 12Daze/DazeEmitterView.m
@@ -47,6 +47,11 @@
 {
     CAEmitterLayer *emitterLayer = (CAEmitterLayer*)self.layer;
     
+    self.backgroundColor = [UIColor colorWithRed:1.0f
+                                           green:0.83f
+                                            blue:0.42f
+                                           alpha:0.9f];
+    
 	emitterLayer.name = @"GlowLayer";
     
 	emitterLayer.emitterShape = kCAEmitterLayerRectangle;
@@ -55,30 +60,65 @@
     
 	emitterLayer.seed = 630879317;
     
-    emitterLayer.lifetime = 1.0;
-    emitterLayer.opacity = 0.9;
+    emitterLayer.lifetime = 1.0f;
+    emitterLayer.opacity = 0.9f;
 	
+    
+    // bubbles!
 	CAEmitterCell *emitterCell = [CAEmitterCell emitterCell];
 	
 	emitterCell.name = @"Glow";
     
 	emitterCell.contents = (id)[[UIImage imageNamed:@"dot.png"] CGImage];
-	emitterCell.contentsRect = CGRectMake(0.00, 0.00, 1.00, 1.00);
+	emitterCell.contentsRect = CGRectMake(0.0, 0.0, 1.0, 1.0);
     
-	emitterCell.scale = .2;
-	emitterCell.scaleRange = .25;
-	emitterCell.scaleSpeed = .9;
+	emitterCell.scale = 0.2f;
+	emitterCell.scaleRange = 0.05f;
+	emitterCell.scaleSpeed = 0.0f;
     
-	emitterCell.color = [[UIColor colorWithRed:1.0 green:0.83 blue:0.42 alpha:0.9] CGColor];
-	emitterCell.alphaRange = 0.05;
-	emitterCell.alphaSpeed = -0.1;
+    emitterCell.velocity = 35.0f;
+    emitterCell.velocityRange = 15.0f;
     
-	emitterCell.lifetime = 10.00;
-	emitterCell.lifetimeRange = 0.00;
-	emitterCell.birthRate = 6;
-	emitterCell.velocity = 0.00;
+    emitterCell.emissionLongitude = -M_PI/2;
+    emitterCell.emissionRange = 0.75f;
     
-	emitterLayer.emitterCells = @[emitterCell];
+	emitterCell.color = [[UIColor colorWithWhite:1.0f alpha:0.75f] CGColor];
+
+	emitterCell.alphaRange = 0.2f;
+	emitterCell.alphaSpeed = -0.05f;
+    
+	emitterCell.lifetime = 10.0f;
+	emitterCell.lifetimeRange = 5.0f;
+	emitterCell.birthRate = 45.0f;
+    
+    
+    // and some ambiance
+    CAEmitterCell *emitterCell2 = [CAEmitterCell emitterCell];
+	
+	emitterCell2.name = @"Glow";
+    
+	emitterCell2.contents = (id)[[UIImage imageNamed:@"dot.png"] CGImage];
+	emitterCell2.contentsRect = CGRectMake(0.0, 0.0, 1.0, 1.0);
+    
+	emitterCell2.scale = 10.0f;
+    
+    emitterCell2.velocity = 0.0f;
+    emitterCell2.velocityRange = 0.0f;
+    
+    emitterCell2.emissionLongitude = -M_PI/2;
+    emitterCell2.emissionRange = 0.75f;
+    
+	emitterCell2.color = [[UIColor colorWithWhite:1.0f alpha:0.1f] CGColor];
+    
+	emitterCell2.alphaRange = 0.1f;
+	emitterCell2.alphaSpeed = -0.005f;
+    
+	emitterCell2.lifetime = 20.0f;
+	emitterCell2.lifetimeRange = 0.0f;
+	emitterCell2.birthRate = 1.0f;
+
+    
+	emitterLayer.emitterCells = @[emitterCell, emitterCell2];
 }
 
 - (void)setFrame:(CGRect)frame
